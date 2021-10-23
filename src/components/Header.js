7b9c138524ef8b90ac9ef49b038b3bc4f41633ac
shreyad216 <shreyad4@illinois.edu>
2021-10-23 14:29:26 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className = "title">Apocalypse Survival Kit</h1>
+        </div>
+    )
+}
+
+export default Header
